8bc8a237f29cde833f58b9d974cb682736a51934
Abhigya Dufare <76903966+abhigyadufare@users.noreply.github.com>
2025-02-21 18:50:02 +0530

    Update maths.ts

--- maths.ts
+++ maths.ts
@@ -7,6 +7,15 @@ add(8, 6);
 
 const minus = (a: number, b: number) => {
     console.log(a - b);
+    return a - b;
 }
 
 minus(8, 6);
+
+
+const multiply = (m: number, n: number) => {
+    console.log(`Multiplying ${m} with ${n} is equals to ${m * n}`)
+    return m * n;
+}
+
+multiply(8, 6);
