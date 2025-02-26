3d509f89c777ede564d409de7466412f2758531a
Abhigya Dufare <76903966+abhigyadufare@users.noreply.github.com>
2025-02-26 18:22:05 +0530

    Create arrays.ts

---
+++ arrays.ts
@@ -0,0 +1,5 @@
+let numbers: number[] = [1, 2, 3];  // Type is explicitly set as an array of numbers
+numbers.push(4);  // This is fine, as 4 is a number
+
+// Accessing array method
+console.log(numbers.length); 
